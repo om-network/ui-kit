17b16825456b3af2cf9d4157658edf6fb88c3015
Patrick Thach <thachp@gmail.com>
2022-01-22 02:40:31 -0800

    Removed margin-top in select field.

--- src/components/fields/Select.field.tsx
+++ src/components/fields/Select.field.tsx
@@ -42,15 +42,13 @@ export const SelectField: React.FunctionComponent<SelectFieldProps> = (props) =>
         <div className={wrapperClasses}>
             <label htmlFor={name} className="block text-sm font-medium text-gray-700">
                 {label}
-                <div className="mt-3">
-                    <select {...register(name)} id={name} className="select" {...selectProps}>
-                        {options.map((item, idx: any) => (
-                            <option key={idx} value={item.value}>
-                                {item.label}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                <select {...register(name)} id={name} className="select" {...selectProps}>
+                    {options.map((item, idx: any) => (
+                        <option key={idx} value={item.value}>
+                            {item.label}
+                        </option>
+                    ))}
+                </select>
             </label>
             {errors[name] && <p className="mt-2 text-sm text-red-500">* {errors[name].message || `${name}: ${validationErrorLabel}.`}</p>}
             {subLabel && <p className="mt-2 text-xs text-gray-500">{subLabel}</p>}
